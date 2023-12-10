78076e1c748611e1fd5c79d78d56aa5008f7aace
Bauyrzhan <galum.bauka03@gmail.com>
2023-12-10 20:51:50 +0600

    Добавьте файлы проекта.

---
+++ Modul16.Practice/ChangeTracker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+class ChangeTracker
+{
+    private string directoryPath;
+    private string logFilePath;
+    private FileSystemWatcher fileSystemWatcher;
+
+    public void Configure(string directoryPath, string logFilePath)
+    {
+        this.directoryPath = directoryPath;
+        this.logFilePath = logFilePath;
+    }
+
+    public void Start()
+    {
+        if (string.IsNullOrEmpty(directoryPath) || string.IsNullOrEmpty(logFilePath))
+        {
+            Console.WriteLine("Конфигурация не завершена. Пожалуйста, выполните настройку отслеживания.");
+            return;
+        }
+
+        fileSystemWatcher = new FileSystemWatcher(directoryPath);
+        fileSystemWatcher.IncludeSubdirectories = true;
+        fileSystemWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+        fileSystemWatcher.Created += OnChanged;
+        fileSystemWatcher.Deleted += OnChanged;
+        fileSystemWatcher.Renamed += OnRenamed;
+
+        fileSystemWatcher.EnableRaisingEvents = true;
+    }
+
+    public void Stop()
+    {
+        if (fileSystemWatcher != null)
+        {
+            fileSystemWatcher.EnableRaisingEvents = false;
+            fileSystemWatcher.Dispose();
+        }
+    }
+
+    private void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        LogChange($"[{DateTime.Now}] {e.ChangeType}: {e.FullPath}");
+    }
+
+    private void OnRenamed(object sender, RenamedEventArgs e)
+    {
+        LogChange($"[{DateTime.Now}] {e.ChangeType}: {e.OldFullPath} -> {e.FullPath}");
+    }
+
+    private void LogChange(string changeInfo)
+    {
+        try
+        {
+            File.AppendAllText(logFilePath, $"{changeInfo}\n");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка логирования: {ex.Message}");
+        }
+    }
+}
---
+++ Modul16.Practice/ChangeTrackerApp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class ChangeTrackerApp
+{
+    private ChangeTracker changeTracker;
+
+    public ChangeTrackerApp()
+    {
+        changeTracker = new ChangeTracker();
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Настройка отслеживания");
+            Console.WriteLine("2. Запуск отслеживания");
+            Console.WriteLine("3. Остановка отслеживания");
+            Console.WriteLine("4. Выход");
+
+            Console.Write("Выберите действие (1-4): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    ConfigureTracking();
+                    break;
+
+                case "2":
+                    StartTracking();
+                    break;
+
+                case "3":
+                    StopTracking();
+                    break;
+
+                case "4":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный выбор. Пожалуйста, выберите снова.");
+                    break;
+            }
+        }
+    }
+
+    private void ConfigureTracking()
+    {
+        Console.Write("Введите путь к отслеживаемой директории: ");
+        string directoryPath = Console.ReadLine();
+        Console.Write("Введите путь к лог-файлу: ");
+        string logFilePath = Console.ReadLine();
+
+        changeTracker.Configure(directoryPath, logFilePath);
+        Console.WriteLine("Конфигурация завершена.");
+    }
+
+    private void StartTracking()
+    {
+        try
+        {
+            changeTracker.Start();
+            Console.WriteLine("Отслеживание запущено.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    private void StopTracking()
+    {
+        changeTracker.Stop();
+        Console.WriteLine("Отслеживание остановлено.");
+    }
+}
